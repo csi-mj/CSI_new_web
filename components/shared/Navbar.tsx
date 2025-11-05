7fdf1a86e6ce55748f96b3f15102b8416dda842b
Feroz <mdferozahmed27156@gmail.com>
2025-11-05 16:49:43 +0530

    merge daniyah

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,18 +5,18 @@ import PillNav from '../PillNav';
 
 const items = [
   { label: 'Home', href: '/home' },
-  { label: 'Team', href: '/team' },
-  { label: 'Events', href: '/events' },
-
   { label: 'About', href: '/about' },
   { label: 'Contact us', href: '/contactus' },
+  { label: 'Team', href: '/team' },
+  { label: 'Events', href: '/events' },
+  { label: 'Resources', href: '/resources' },
   { label: 'Become a Member', href: '/membership' }
 ];
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 right-0 left-0 z-50 flex w-full justify-center px-4">
-      
+
       <div className="mt-3 flex h-[62px] max-w-fit items-center rounded-full border border-white/[0.12] bg-white/5 px-[3px] shadow-[0_8px_32px_0_rgba(0,0,0,0.6)] backdrop-blur-md z-50">
         <PillNav
           logo="/logo.png"
@@ -31,7 +31,7 @@ const Navbar = () => {
           initialLoadAnimation={true}
         />
       </div>
-      
+
     </nav>
   );
 };
